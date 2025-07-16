72206d7402d4c40418884e03cc3eca3d38f2b1e0
Lauren <129151611+mirial83@users.noreply.github.com>
2025-07-16 17:53:47 -0400

    added Waypoint by Lunarwater check

--- Mirial/DeedLore/DL_Main.lua
+++ Mirial/DeedLore/DL_Main.lua
@@ -10,6 +10,29 @@ function print(text) Turbine.Shell.WriteLine("<rgb=#00FFFF>DL:</rgb> "..text) en
 function printh(text) print("<rgb=#00FF00>"..text.."</rgb>") end
 function printe(text) print("<rgb=#FF6040>Error: "..text.."</rgb>") end
 
+-- check for Waypoint
+					local tmpPlugins=Turbine.PluginManager.GetAvailablePlugins();
+					local pluginIndex;
+					for pluginIndex=1,#tmpPlugins do
+						if tmpPlugins[pluginIndex].Name=="Waypoint" then
+							WaypointInstalled=true;
+							break;
+						end
+					end
+					if WaypointInstalled then
+						tmpPlugins=Turbine.PluginManager.GetLoadedPlugins();
+						for pluginIndex=1,#tmpPlugins do
+							if tmpPlugins[pluginIndex].Name=="Waypoint" then
+								WaypointRunning=true;
+								break;
+							end
+						end
+							if (not WaypointRunning) then
+							Turbine.PluginManager.LoadPlugin("Waypoint");
+							WaypointRunning=true;
+						end
+					end
+
 local lPat = "You are on %a* server "
 local locPat = "You are on %a* server %d* at r(%d) lx%d+ ly%d+ ox.-%d+%.?%d* oy.-%d+%.?%d* oz(.-%d+%.?%d*)"
 local liPat = "You are on %a* server %d* at r(%d) lx%d+ ly%d+ i%d* ox.-%d+%.?%d* oy.-%d+%.?%d* oz(.-%d+%.?%d*)"
